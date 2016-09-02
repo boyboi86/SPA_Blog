431b34e4b03afc454e3fedcdfee2da31c1e1f675
boyboi86 <boyboi86@gmail.com>
2016-09-02 15:44:58 +0800

    added action creator with axios

---
+++ src/actions/index.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+export const FETCH_POSTS = 'FETCH_POSTS';
+
+const BASE_URL = 'http://reduxblog.herokuapp.com/api';
+const API_KEY = '?key=API_KEY4Blog';
+
+export fetchPosts() {
+
+const request = axios.get(`${BASE_URL}/posts${API_KEY}`);
+
+  return {
+    type: FETCH_POSTS,
+    payload: request
+  };
+}
