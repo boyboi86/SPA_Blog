0ee0439d73b29bcd502d86c93e3a29d221055d41
boyboi86 <boyboi86@gmail.com>
2016-09-02 15:46:08 +0800

    setting up index routes

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import { Router, IndexRoute } from 'react-router';
 
-import App from './components/app.js';
+import App from './components/app';
+import PostsIndex from './components/posts_index';
 
 export default (
-<Router path="/" component={App} />
+<Router path="/" component={App} >
+  <IndexRoute component={PostsIndex} />
+</Router>
 );
