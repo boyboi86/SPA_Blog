a4e23178a78b647a83fd3b8d2b8b98a3f1a4f21c
boyboi86 <boyboi86@gmail.com>
2016-09-02 16:56:53 +0800

    action creator update

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
 export const FETCH_POSTS = 'FETCH_POSTS';
-
+// Blog API from external source
 const BASE_URL = 'http://reduxblog.herokuapp.com/api';
 const API_KEY = '?key=API_KEY4Blog';
 
-export fetchPosts() {
+export function fetchPosts() {
 
 const request = axios.get(`${BASE_URL}/posts${API_KEY}`);
 
