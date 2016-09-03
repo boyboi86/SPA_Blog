021d23a69fa1292115e3fabae050c0edc2b5020b
boyboi86 <boyboi86@gmail.com>
2016-09-03 20:11:23 +0800

    updated form with redux

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -5,19 +5,20 @@ class PostsNew extends Component {
 
 
   render(){
+    const { fields: { title, categories, content }, handleSubmit } = this.props;
     return (
-      <form>
+      <form onSubmit={ handleSubmit }>
         <div className="form-group">
           <label>Title</label>
-          <input type="text" className="form-control" />
+          <input type="text" className="form-control" { ...title } />
         </div>
         <div className="form-group">
           <label>Categories</label>
-          <input type="text" className="form-control" />
+          <input type="text" className="form-control" { ...categories } />
         </div>
         <div className="form-group">
           <label>Content</label>
-          <textarea className="form-control" />
+          <textarea className="form-control" { ...content }/>
         </div>
         <button type="submit" className="btn btn-primary">Submit</button>
       </form>
