5039f415cd2401dafc6b4818a1c281cacd7928db
boyboi86 <boyboi86@gmail.com>
2016-09-03 21:35:47 +0800

    update all dispatch for forms

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
 
 export const FETCH_POSTS = 'FETCH_POSTS';
+export const CREATE_POST = 'CREATE_POST';
 // Blog API from external source
-const BASE_URL = 'http://reduxblog.herokuapp.com/api';
+const BASE_URL = 'https://reduxblog.herokuapp.com/api';
 const API_KEY = '?key=API_KEY4Blog';
 
 export function fetchPosts() {
@@ -11,6 +12,16 @@ const request = axios.get(`${BASE_URL}/posts${API_KEY}`);
 
   return {
     type: FETCH_POSTS,
+    payload: request
+  };
+}
+
+export function createPost(props) {
+
+  const request = axios.post(`${BASE_URL}/posts${API_KEY}`, props);
+
+  return {
+    type: CREATE_POST,
     payload: request
   };
 }
--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import { reduxForm } from 'redux-form';
+import { createPost } from '../actions/index';
 
 class PostsNew extends Component {
-
-
   render(){
     const { fields: { title, categories, content }, handleSubmit } = this.props;
     return (
-      <form onSubmit={ handleSubmit }>
+      <form onSubmit={ handleSubmit(this.props.createPost) }>
+      <h3>New Post</h3>
         <div className="form-group">
           <label>Title</label>
           <input type="text" className="form-control" { ...title } />
@@ -26,7 +26,14 @@ class PostsNew extends Component {
   }
 }
 
+function validate(values){
+  const errors = {};
+
+  return errors;
+}
+
+// redux-form is similar to connect except first param is form config
 export default reduxForm({
   form: 'PostsNewForm',
   fields: ['title', 'categories', 'content']
-})(PostsNew);
+}, null, { createPost })(PostsNew);
