7d783d99d88276113b6b16896a4ffb84c860e30f
boyboi86 <boyboi86@gmail.com>
2016-09-04 19:55:01 +0800

    update actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 export const FETCH_POSTS = 'FETCH_POSTS';
 export const CREATE_POST = 'CREATE_POST';
 export const FETCH_POST = 'FETCH_POST';
+export const DELETE_POST = 'DELETE_POST';
 
 /* Blog API from external source*/
 const BASE_URL = 'https://reduxblog.herokuapp.com/api';
@@ -36,4 +37,14 @@ export function fetchPost(id) {
     type: FETCH_POST,
     payload: request
   };
+}
+
+export function deletePost(id) {
+
+  const request = axios.delete(`${BASE_URL}/posts/${id}${API_KEY}`, id);
+
+  return {
+    type: DELETE_POST,
+    payload: request
+  }
 }
